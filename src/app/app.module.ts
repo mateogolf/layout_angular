464a8b859a5acb14afd2f60922b2151213dba73a
mateogolf <rodriguezml11@mail.wlu.edu>
2017-10-12 15:40:28 -0400

    Layout assignment all CSS-ified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { HeaderCompComponent } from './header-comp/header-comp.component';
+import { NavCompComponent } from './nav-comp/nav-comp.component';
+import { MainCompComponent } from './main-comp/main-comp.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderCompComponent,
+    NavCompComponent,
+    MainCompComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule, 
+    HttpModule
   ],
   providers: [],
   bootstrap: [AppComponent]
